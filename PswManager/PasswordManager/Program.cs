d2b0cc217c71a78f27d0105a04774f01a693c2f8
Eo-Le-LearnToHack <101599426+Eo-Le-LearnToHack@users.noreply.github.com>
2022-07-18 13:45:41 +0200

    Add files via upload

---
+++ PswManager/PasswordManager/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool loopProgramMain = true;
+            do
+            {
+                //CheckIfMasterPswExistOrNotInDatabase
+                //IfMasterPswNotExist ThenCreateNewMasterPsw
+                //IfMasterPswExist ThenAskForPsw LoginAttempt3Times
+                //IfCorrectMasterPswEnteren ThenEnterTheProgramPasswordManager
+
+                //CheckIfAnySQLTableOtherThanMasterPswExist
+                //IfNotExist ThenAskForCreateOfNewTable/Service
+                //IfExist ThenListAllTheServices and options available
+
+
+
+            } while (loopProgramMain);
+
+
+
+
+
+
+
+        }//Main(string[] args)
+    }//class Program
+}//namespace PasswordManager
