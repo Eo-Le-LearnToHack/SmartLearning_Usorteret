ad5def8ed5cb263276bfbf667b7dd08f1f04ed67
Eo-Le-LearnToHack <101599426+Eo-Le-LearnToHack@users.noreply.github.com>
2022-07-18 13:45:13 +0200

    Add files via upload

---
+++ PswManager/LoginForm00/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using PasswordManager;
+
+namespace LoginForm00
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RegisterUser MyRegisterUser = new RegisterUser(); //
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void txtBox_Username_MouseLeave(object sender, MouseEventArgs e)
+        {
+            txtBox_Username.SetCurrentValue(BackgroundProperty, Brushes.Transparent);
+            icon_Account.SetCurrentValue(BackgroundProperty, Brushes.Transparent);
+        }
+
+        private void txtBox_Username_MouseEnter(object sender, MouseEventArgs e)
+        {
+            txtBox_Username.SetCurrentValue(BackgroundProperty, Brushes.DeepSkyBlue);
+            icon_Account.SetCurrentValue(BackgroundProperty, Brushes.DeepSkyBlue);
+        }
+
+
+        private void PswBox_MouseLeave(object sender, MouseEventArgs e)
+        {
+            PswBox.SetCurrentValue(BackgroundProperty, Brushes.Transparent);
+            icon_Key.SetCurrentValue(BackgroundProperty, Brushes.Transparent);
+        }
+
+        private void PswBox_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PswBox.SetCurrentValue(BackgroundProperty, Brushes.DeepSkyBlue);
+            icon_Key.SetCurrentValue(BackgroundProperty, Brushes.DeepSkyBlue);
+        }
+    }
+}
---
+++ PswManager/LoginForm00/ReadMe.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginForm00
+{
+    internal class ReadMe
+    {
+
+        /*
+        C# WPF | Login & Register Window | Modern Design UI [With Source Code]: https://www.youtube.com/watch?v=MWVfsLOhUXM
+        //Installing Icon Nucket Package, "Install-Package MahApps.Metro.IconPacks.Material -Version 4.11.0" from source https://github.com/MahApps/MahApps.Metro.IconPacks
+        //Adding nucket package to project: https://www.youtube.com/watch?v=nMTkakFVfcg
+
+
+
+        Image not displaying at runtime C# WPF: https://stackoverflow.com/questions/24049511/image-not-displaying-at-runtime-c-sharp-wpf
+
+        
+         Using namespace from another project:  https://stackoverflow.com/questions/4416879/vs2010-use-namespace-from-another-project-within-the-solution
+         
+
+        xmlns:local="clr-namespace:LiveLearn24062022" xmlns:iconPacks="clr-namespace:MahApps.Metro.IconPacks;assembly=MahApps.Metro.IconPacks.Material"
+        <Window x:Class="LiveLearn24062022.Window1"
+        xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
+        xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns:local="clr-namespace:LiveLearn24062022" xmlns:iconPacks="clr-namespace:MahApps.Metro.IconPacks;assembly=MahApps.Metro.IconPacks.Material"
+        mc:Ignorable="d"
+        Title="Window1" Height="450" Width="800">
+   
+
+    <Grid>
+        <iconPacks:PackIconMaterial Kind="EmoticonCool" VerticalAlignment="Center" HorizontalAlignment="Center" />
+    </Grid>
+</Window>
+
+
+         */
+
+    }
+}
